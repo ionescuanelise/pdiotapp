2a510912458f592dff32ef89e06fc17255e75f4f
anelise.maria <ionescu.anelise@gmail.com>
2021-10-20 02:57:17 +0100

    first demo code

--- app/src/main/java/com/specknet/pdiotapp/live/LiveDataActivity.kt
+++ app/src/main/java/com/specknet/pdiotapp/live/LiveDataActivity.kt
@@ -10,16 +10,30 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
 import android.util.Log
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.specknet.pdiotapp.MainActivity
 import com.specknet.pdiotapp.R
+import com.specknet.pdiotapp.ml.CnnHarV1
 import com.specknet.pdiotapp.utils.Constants
 import com.specknet.pdiotapp.utils.RESpeckLiveData
+import com.specknet.pdiotapp.utils.RespeckDataPrediction
 import com.specknet.pdiotapp.utils.ThingyLiveData
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.util.stream.Collectors
+import java.util.stream.Stream
 import kotlin.collections.ArrayList
 
 
@@ -37,6 +51,9 @@ class LiveDataActivity : AppCompatActivity() {
     var time = 0f
     lateinit var allRespeckData: LineData
 
+    lateinit var predictionTextView : TextView
+    lateinit var activityPrediction : String
+
     lateinit var allThingyData: LineData
 
     lateinit var respeckChart: LineChart
@@ -51,9 +68,86 @@ class LiveDataActivity : AppCompatActivity() {
     val filterTestRespeck = IntentFilter(Constants.ACTION_RESPECK_LIVE_BROADCAST)
     val filterTestThingy = IntentFilter(Constants.ACTION_THINGY_BROADCAST)
 
+    var predictions = ArrayList<FloatArray>()
+
+
+    @Throws(IOException::class)
+    private fun loadModelFile(tflite : String): MappedByteBuffer{
+        val MODEL_ASSETS_PATH = tflite
+        val assetFileDescrptor = assets.openFd(MODEL_ASSETS_PATH)
+        val fileInputStream = FileInputStream(assetFileDescrptor.getFileDescriptor())
+        val fileChannel = fileInputStream.getChannel()
+        val startOffset = assetFileDescrptor.startOffset
+        val declaredLength = assetFileDescrptor.getDeclaredLength()
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+    }
+
+
+    val map : HashMap<Int, String> = HashMap<Int, String>()
+
+    fun createMap(){
+        map.put(0, "Climbing stairs");
+        map.put(1, "Descending stairs");
+        map.put(2, "Desk work");
+        map.put(3, "Falling on knees");
+        map.put(4, "Falling on the back");
+        map.put(5, "Falling on the left");
+        map.put(6, "Falling on the right");
+        map.put(7, "Lying down left");
+        map.put(11, "Movement");
+        map.put(12, "Running");
+        map.put(13, "Sitting");
+        map.put(14, "Sitting bent backward");
+        map.put(15, "Sitting bent forward");
+        map.put(8, "Lying down on back");
+        map.put(9, "Lying down on stomach");
+        map.put(10, "Lying down right");
+        map.put(16, "Standing");
+        map.put(17, "Walking at normal speed");
+    }
+
+    fun mapOutputLabel(maxIndex : Int) : String {
+        return map.get(maxIndex).toString()
+    }
+
+
+    val interpreter = Interpreter(loadModelFile("app/src/main/ml/CNN_HAR_v1.tflite"))
+
+
+    fun getActivityPredictionString(window : Array<FloatArray>): String {
+        val output = FloatArray(18)
+        interpreter.run(arrayOf(window), arrayOf(output))
+        val maxIndex = output.indices.maxBy { output[it] } ?: -1
+        val resultString = "Activity is: " + mapOutputLabel(maxIndex)
+        return resultString
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_live_data)
+        createMap()
+
+        predictionTextView = findViewById(R.id.activityPredictionTextView)
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    while (!this.isInterrupted) {
+                        sleep(2000)
+                        runOnUiThread {
+                            activityPrediction = getActivityPredictionString(predictions.toTypedArray())
+                            predictionTextView.setText(activityPrediction)
+                            predictions.clear()
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                }
+            }
+        }
+
+        thread.start()
 
         setupCharts()
 
@@ -75,6 +169,10 @@ class LiveDataActivity : AppCompatActivity() {
                     val x = liveData.accelX
                     val y = liveData.accelY
                     val z = liveData.accelZ
+
+                    val g = liveData.gyro
+
+                    predictions.add(floatArrayOf(x, y, z, g.x, g.y, g.z))
 
                     time += 1
                     updateGraph("respeck", x, y, z)
@@ -248,7 +346,6 @@ class LiveDataActivity : AppCompatActivity() {
 
 
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
