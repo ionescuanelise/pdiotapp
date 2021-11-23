137ed2b497ee26943c38416487e3dd7ce7130bbc
anelise.maria <ionescu.anelise@gmail.com>
2021-11-23 14:43:26 +0000

    graph running

--- app/src/main/java/com/specknet/pdiotapp/ChartRunning.kt
+++ app/src/main/java/com/specknet/pdiotapp/ChartRunning.kt
@@ -1,11 +1,103 @@
 package com.specknet.pdiotapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.specknet.pdiotapp.utils.DataPointsGraph
 
 class ChartRunning: AppCompatActivity() {
+
+    private lateinit var barChart: BarChart
+    private var TAG = "FragmentActivity"
+    private var scoreList = ArrayList<DataPointsGraph>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chart_running)
+
+        barChart = findViewById(R.id.barChart)
+
+        scoreList = getScoreList()
+
+        initBarChart()
+
+        //now draw bar chart with dynamic data
+        val entries: ArrayList<BarEntry> = ArrayList()
+
+        //you can replace this data object with  your custom object
+        for (i in scoreList.indices) {
+            val score = scoreList[i]
+            entries.add(BarEntry(i.toFloat(), score.time))
+        }
+
+        val barDataSet = BarDataSet(entries, "")
+        barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+
+        val data = BarData(barDataSet)
+        barChart.data = data
+
+        barChart.invalidate()
+
     }
+
+    private fun initBarChart() {
+        //hide grid lines
+        barChart.axisLeft.setDrawGridLines(false)
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(false)
+
+        //remove right y-axis
+        barChart.axisRight.isEnabled = false
+
+        //remove legend
+        barChart.legend.isEnabled = false
+
+        //remove description label
+        barChart.description.isEnabled = false
+
+        //add animation
+        barChart.animateY(3000)
+
+        // to draw label on xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
+        xAxis.valueFormatter = MyAxisFormatter()
+        xAxis.setDrawLabels(true)
+        xAxis.granularity = 1f
+        xAxis.labelRotationAngle = +90f
+
+    }
+
+    inner class MyAxisFormatter : IndexAxisValueFormatter() {
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            val index = value.toInt()
+            Log.d(TAG, "getAxisLabel: index $index")
+            return if (index < scoreList.size) {
+                scoreList[index].day
+            } else {
+                ""
+            }
+        }
+    }
+
+    // simulate api call
+    // we are initialising it directly
+    private fun getScoreList(): ArrayList<DataPointsGraph> {
+        scoreList.add(DataPointsGraph("Mon", 56.0F))
+        scoreList.add(DataPointsGraph("Tue", 75.0F))
+        scoreList.add(DataPointsGraph("Wed", 85.0F))
+        scoreList.add(DataPointsGraph("Thu", 45.0F))
+        scoreList.add(DataPointsGraph("Fri", 63.0F))
+
+        return scoreList
+    }
+
 }
---
+++ app/src/main/java/com/specknet/pdiotapp/utils/DataPointsGraph.kt
@@ -0,0 +1,3 @@
+package com.specknet.pdiotapp.utils
+
+data class DataPointsGraph (val day: String, val time: Float)
